ad062f82d2c59434a1b903d8adf3b426755a846c
Robbe Vanslambrouck <robbevsb.dev@gmail.com>
2022-05-29 23:58:30 +0200

    number buttons work

--- script.js
+++ script.js
@@ -1,113 +1,8 @@
-const numberBtns = document.querySelectorAll(".number");
-const btnBackspace = document.querySelector("#backspace");
-const txtCurrentLine = document.querySelector("#currentLine");
-
-class ScreenLine {
-    #domElement = document;
-    #text
-    constructor(domElement, textContent="") {
-        this.#domElement = domElement;
-        this.#text = textContent;
-    }
-    update() {
-        this.#domElement.textContent = this.#text;
-    }
-    addChar(char) {
-        this.#text += char;
-    }
-    removeLastChar() {
-        this.#text = this.#text.slice(0, -1);
-    }
-    set text(string) {
-        this.#text = string;
-    }
-
-    get text() {
-        return this.#text;
-    }
-}
-
-class Screen {
-    lines = [];
-    constructor(lines) {
-        this.lines = lines;
-    }
-    update() {
-        Object.values(this.lines).forEach(line => {
-            line.update();
-        });
-    }
-
-    clear() {
-        Object.values(this.lines).forEach(line => {
-            line.text = "";
-        });
-    }
-}
-
-class Operation {
-    a;
-    b;
-    operator;
-    solution = 0;
-    constructor(a = NaN, b = NaN, operator = "") {
-        this.a = a;
-        this.b = b;
-        this.operator = operator;
-    }
-
-
-
-    set operator(operator) {
-        this.operator = operator;
-    }
-
-    isSolvable() {
-        return  !isNaN(this.a) && !isNaN(this.b) && this.operator != "";
-    }
-
-    solved() {
-        if(this.isSolvable()) {
-            this.solution = operate(this.operator, this.a, this.b);
-            return true;
-        }
-        return false;
-    }
-
-    addNumber(number) {
-        if (isNaN(this.a)) {
-            this.a = number;
-            return;
-        }
-        this.b = number;
-    }
-
-    toString() {
-        if (isNaN(this.a)) {
-            return "Error"
-        }
-        if (isNaN(this.b)) {
-            return this.a + " " + this.operator;
-        }
-
-        return this.a + " " + this.operator + " " + this.b + " =";
-    }
-
-}
-
-const screenLines = {
-    current: new ScreenLine(document.querySelector("#currentLine")), 
-    history: new ScreenLine(document.querySelector("#historyLine"))
-};
-
-const screen = new Screen(screenLines);
-
-let operation = new Operation();
-
-const input = {
+const buttons = {
     allClear: document.querySelector("#allClear"),
     backspace: document.querySelector("#backspace"),
     solve: document.querySelector("#solve"),
+    dot: document.querySelector("#dot"),
     numbers: {
         zero: document.querySelector("#zero"),
         one: document.querySelector("#one"),
@@ -121,77 +16,66 @@ const input = {
         nine: document.querySelector("#nine"),
     },
     operators: {
-        "+": document.querySelector("#add"),
-        "-": document.querySelector("#subtract"),
-        "*": document.querySelector("#multiply"),
-        "/": document.querySelector("#divide"),
-        "%": document.querySelector("#percent"),
+        add: document.querySelector("#add"),
+        sub: document.querySelector("#subtract"),
+        mul: document.querySelector("#multiply"),
+        div: document.querySelector("#divide"),
+        percent: document.querySelector("#percent"),
     }
 }
 
-input.allClear.addEventListener("click", e => {
-    screen.clear();
-    screen.update();
+const display = {
+    stat: document.querySelector("#statusLine"),
+    main: document.querySelector("#mainLine")
+}
+
+const numbers = {
+    a: NaN,
+    b: NaN
+}
+
+let operator = "";
+
+
+buttons.allClear.addEventListener("click", e => {
+    clearDisplay();
+    numbers.a = NaN;
+    numbers.b = NaN;
+    operator = "";
 });
 
-input.backspace.addEventListener("click", e => {
-    screen.lines.current.removeLastChar();
-    screen.update();
+buttons.backspace.addEventListener("click", e => {
+    let text = display.main.textContent;
+    text = text.slice(0, -1);
+    if (text === "") {
+        text = "0";
+    }
+    display.main.textContent = text;
 });
- 
-Object.values(input.numbers).forEach(number => {
-    number.addEventListener("click", e => {
-        screen.lines.current.addChar(e.target.textContent);
-        screen.update();
+
+buttons.dot.addEventListener("click", e => {
+    text = display.main.textContent;
+    if (!text.includes(".")) {
+        text += ".";
+        display.main.textContent = text;
+    }
+});
+
+Object.values(buttons.numbers).forEach(btn => {
+    btn.addEventListener("click", e => {
+        let text = display.main.textContent;
+        text += e.target.textContent;
+        text = text.replace(/^0+(?!\.)/, "");
+        if (text === "") {
+            text = "0";
+        }
+        display.main.textContent = text;
     });
 });
 
-for (const [op, domElement] of Object.entries(input.operators)) {
-    domElement.addEventListener("click", e => {
-        executeOperation(op);
-    });
-}
-
-input.solve.addEventListener("click", e => {
-    let currNum = parseInt(screen.lines.current.text);
-    if (isNaN(currNum)) {
-        return;
-    }
-    operation.addNumber(currNum);
-    if (operation.solved()) {
-        let answer = operation.solution
-        screen.lines.history.text = operation.toString();
-        screen.lines.current.text = answer;
-        screen.update();
-        operation = new Operation();
-    }
-});
-
-
-
-function executeOperation(op) {
-    operation.operator = op;
-    let currNum = parseInt(screen.lines.current.text);
-    if (isNaN(currNum)) {
-        screen.lines.history.text = operation.toString();
-        screen.lines.history.update();
-        return;
-    }
-    operation.addNumber(currNum);
-    if (operation.solved()) {
-        let answer = operation.solution;
-        operation = new Operation();
-        operation.a = answer;
-        operation.operator = op;
-        screen.lines.current.text = answer;
-    }
-    screen.lines.history.text = operation.toString();
-    screen.update();
-    screen.lines.current.text = "";
-}
-
-function init() {
-    screen.update();
+function clearDisplay() {
+    display.main.textContent = 0;
+    display.stat.textContent = "";
 }
 
 function add(a, b=0) {
@@ -229,4 +113,4 @@ function operate(operator, a, b) {
     }
 }
 
-init();
+clearDisplay();
